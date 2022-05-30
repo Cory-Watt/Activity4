426974972aa21d5860a93ea99537752fd6570335
Cory-Watt <106133667+Cory-Watt@users.noreply.github.com>
2022-05-30 02:25:38 -0400

    Add files via upload

---
+++ WinFormAct4/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WinFormAct4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.conversion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the number of seconds elapsed: ";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(16, 77);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 1;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // userInput
+            // 
+            this.userInput.Location = new System.Drawing.Point(226, 13);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(100, 20);
+            this.userInput.TabIndex = 2;
+            // 
+            // conversion
+            // 
+            this.conversion.Location = new System.Drawing.Point(226, 80);
+            this.conversion.Name = "conversion";
+            this.conversion.Size = new System.Drawing.Size(100, 20);
+            this.conversion.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 125);
+            this.Controls.Add(this.conversion);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Welcome!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.TextBox conversion;
+    }
+}
+
---
+++ WinFormAct4/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormAct4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            int numSeconds = int.Parse(userInput.Text);
+            if (numSeconds >= 86400)
+            {
+                int numDays = numSeconds / 86400;
+                conversion.Text = numDays + " days";
+            }
+            else if (numSeconds >= 3600)
+            {
+                int numHours = numSeconds / 3600;
+                conversion.Text = numHours + " hour(s)";
+            }
+            else if (numSeconds >= 60)
+            {
+                int numMinutes = numSeconds / 60;
+                conversion.Text = numMinutes + " minutes";
+            }
+        }
+    }
+   }
+
